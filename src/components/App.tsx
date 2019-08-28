57963f3861e4a3431045c26df7f1d782021d677d
Victor Joh <wantechjoh@gmail.com>
2019-08-28 03:06:09 -0500

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Node from "./Node";
+
+const myArray = [1, 20, -309, 23, 80, -40, 483, 829, 984, 34, 3, -20, -203]
+
+interface IState {
+  node?: any
+}
+
+class App extends React.Component<any, IState>{
+  constructor(props: any){
+    super(props)
+
+    this.state = {}
+  }
+
+  binarySearchTree() {
+    const node = new Node(myArray[0])
+    const tree = myArray.map((number, index) => {
+      node.insert(number);
+    })
+    this.setState({ node })
+  }
+
+  containBST() {
+    
+  }
+
+  componentDidMount() {
+    this.binarySearchTree();
+  }
+
+  render(){
+    console.log(this.state.node)
+    return(
+      <div>
+        Hello!
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Node.tsx
@@ -0,0 +1,37 @@
+import { tsParenthesizedType } from "@babel/types";
+
+export default class Node {
+  private _data: number;
+  private _left?: Node;
+  private _right?: Node;
+
+  constructor(_data: number) {
+    this._data = _data;
+  }
+
+  public insert(data: number) {
+    if (data < this._data && this._left) {
+      this._left.insert(data); //recursive if left node exists then go down to next child
+    } else if (data < this._data) {
+      this._left = new Node(data); //if left node doesn't exist, assign new left node
+    } else if (data > this._data && this._right) {
+      this._right.insert(data); //recursive for right
+    } else if (data > this._data) {
+      this._right = new Node(data); //right insert new node
+    }
+  }
+
+  public contains(data: number) {
+    if (this._data === data) {
+      return this;
+    }
+
+    if (this._data < data && this._right) {
+       this._right.contains(data);
+    } else if (this._data > data && this._left) {
+       this._left.contains(data);
+    }
+
+    return null;
+  }
+}
